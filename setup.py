2ca1b97ab7a3b3d2ce6d89964bdd3fcf44d729f6
liamcripwell <ljcripwell@gmail.com>
2021-10-25 13:32:20 +0200

    make setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cryptpad_generation",
+    version="0.0.1",
+    author="Liam Cripwell",
+    description="A tool to automate the generation of CryptPad forms.",
+    packages=find_packages(),
+    include_package_data=True,
+)
